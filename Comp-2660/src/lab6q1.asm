4e964909873b59e5d096cd08ad0b5a59f11040e2
Edward Nafornita <eddie.nafornita@gmail.com>
2023-03-18 14:47:46 -0400

    Removed temporary file

---
+++ Comp-2660/src/lab6q1.asm
@@ -0,0 +1,26 @@
+TITLE Lab 6 Activity 1
+
+INCLUDE Irvine32.inc
+INCLUDELIB Irvine32.lib
+INCLUDELIB kernel32.lib
+INCLUDELIB user32.lib
+
+.data
+array sword -3, -6, -1, -10, 10, 30, 40, 4
+sentinel sword 0
+
+.code
+main proc
+    mov esi, offset array
+    mov ecx, lengthof array
+
+    L1:
+        test word ptr [esi], 8000h
+        pushfd
+        add esi, type array
+        popfd
+    loopnz L1
+    call DumpRegs
+    exit
+main endp
+end main
