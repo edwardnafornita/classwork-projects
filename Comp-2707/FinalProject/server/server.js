d50e20bb8f7130f311d1c3a63b75957f863a46aa
Edward Nafornita <eddie.nafornita@gmail.com>
2023-04-03 13:30:05 -0400

    Removed Final Project from Comp2707

--- Comp-2707/FinalProject/server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get("/api", (request, response) => {
-    response.json({"users":["admin", "instructor", "student"]})
-});
-
-app.listen(5000, () => {console.log("Server started on port 5000")});
